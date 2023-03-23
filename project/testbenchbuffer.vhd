35d3eeb0fd76909e108db32f0bc9c7892a8f2124
jeandtx <jean.doutriaux@gmail.com>
2023-03-23 17:15:47 +0100

    better testing of buffer

--- project/testbenchbuffer.vhd
+++ project/testbenchbuffer.vhd
@@ -8,8 +8,6 @@ entity mybufferNbitstestbench is
 end mybufferNbitstestbench;
 
 architecture mybufferNbitstestbench_Arch of mybufferNbitstestbench is
-
-	--Déclaration du composant à tester -> renvoie vers l'entité bufferNbits
 	component bufferNbits is
 	generic (N : integer );
 	port(
@@ -21,21 +19,17 @@ architecture mybufferNbitstestbench_Arch of mybufferNbitstestbench is
 	);
 	end component;
 
-	--Déclaration de la constante pour le paramètre générique (non obligatoire)
 	constant N : integer := 3;
 
-    constant PERIOD : time := 100 us;
+    constant PERIOD : time := 50 us;
 
-	--Déclaration des signaux internes à l'architecture pour réaliser les simulations
 	signal e1_sim : std_logic_vector(N-1 downto 0) := (others => '0');
-	signal s1_sim, s1_calc : std_logic_vector(N-1 downto 0) := (others => '0');
+	signal s1_sim : std_logic_vector(N-1 downto 0) := (others => '0');
 	signal reset_sim, preset_sim, clock_sim : std_logic := '0';
 
 begin
 
-	--Instanciation du composant à tester
 	MyComponentsynthcombo1underTest : bufferNbits
-	--raccordement des ports du composant aux signaux dans l'architecture
 	generic map(N =>N)
 	port map (
 		e1 => e1_sim,
@@ -50,35 +44,34 @@ begin
         wait for PERIOD/2;
         clock_sim <= '1';
         wait for PERIOD/2;
-        if now = 20 * PERIOD then
-            wait;
-        end if;
     end process;
 
     MyStimulus_Proc2: process
     begin
-        reset_sim <= '1';
-        preset_sim <= '0';
-        e1_sim <= (others => '0');
-        s1_calc <= (others => '0');
-        wait for 3.25*PERIOD;
-        report "reset = " & std_logic'image(reset_sim) & " preset = " & std_logic'image(preset_sim) & " e1 = " & integer'image(to_integer(unsigned(e1_sim))) & " s1 = " & integer'image(to_integer(unsigned(s1_sim)));
-
-        reset_sim <= '0';
-        preset_sim <= '1';
-        e1_sim <= (others => '0');
-        s1_calc <= (others => '1');
-        wait for 3.5*PERIOD;
-        report "reset = " & std_logic'image(reset_sim) & " preset = " & std_logic'image(preset_sim) & " e1 = " & integer'image(to_integer(unsigned(e1_sim))) & " s1 = " & integer'image(to_integer(unsigned(s1_sim)));
-
-        reset_sim <= '0';
-        preset_sim <= '0';
         for i in 0 to (2**N)-1 loop
             e1_sim <= std_logic_vector(to_unsigned(i,N));
             wait for PERIOD;
-            report "reset = " & std_logic'image(reset_sim) & " preset = " & std_logic'image(preset_sim) & " e1 = " & integer'image(to_integer(unsigned(e1_sim))) & " s1 = " & integer'image(to_integer(unsigned(s1_sim)));
         end loop;
-        report "Test ok (no assert ...)";
+        for i in 0 to (2**N)-1 loop
+            e1_sim <= std_logic_vector(to_unsigned(i,N));
+            wait for PERIOD;
+        end loop;
+        wait;
+    end process;
+    My_Reset_Proc: process
+    begin
+        wait for 234 us;
+        reset_sim <= '1';
+        wait for 150 us;
+        reset_sim <= '0';
+        wait;
+    end process;
+    My_Preset_Proc: process
+    begin
+        wait for 610 us;
+        preset_sim <= '1';
+        wait for 150 us;
+        preset_sim <= '0';
         wait;
     end process;
 end mybufferNbitstestbench_Arch;
